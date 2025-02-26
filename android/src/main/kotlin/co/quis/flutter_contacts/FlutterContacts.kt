459a2b5e7ae9d594146fef9cb6aa6201c82fc876
Nimit Sanja <nimit.sanja@esparkbizmail.com>
2025-02-26 15:27:37 +0530

    add param

--- android/src/main/kotlin/co/quis/flutter_contacts/FlutterContacts.kt
+++ android/src/main/kotlin/co/quis/flutter_contacts/FlutterContacts.kt
@@ -793,9 +793,8 @@ class FlutterContacts {
 
                 val id = if (idIndex != -1) cursor.getString(idIndex) ?: "" else ""
                 val name = if (nameIndex != -1) cursor.getString(nameIndex) ?: "" else ""
-                val isStarred = false
 
-                contacts.add(Contact(id, name, isStarred))
+                contacts.add(Contact(id, name, null,false))
             }
 
             cursor.close()
